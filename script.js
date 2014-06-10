c4f57bacb3f76e39c23d9518d2b5d826f34656b3
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-06-10 16:30:10 -0700

    finished app

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
 $(function() {
-
   $('#searchButton').on("click", function(e){
     e.preventDefault();
+    clearResults();
     var userInput = $('#searchTerm').val();
     // console.log(userInput);
+    $('#searchTerm').val("");
 
     var request = {
       url: "http://www.omdbapi.com/",
@@ -17,22 +18,51 @@ $(function() {
     var response = $.ajax(request);
 
     response.done(function(data){
+      console.log(data);
       $.each(data["Search"], function (index,movie){
         // console.log(movie["Title"]);
-      $('#results').append("<li>" + movie["Title"] + "</li>");
+      $('#results').append("<li data-imdbid = " + movie["imdbID"] +"> " + movie["Title"] + "</li>");
+      //li.data('imdbid', movie["imdbID"]);---same as adding to li above
+      // var movieID = movie["imdbID"];
+      // console.log(movieID);
       })
-
     })
-
-
-
-
-
-
-
   })
 
+  $('#results').delegate("li","click", function(e){
+    e.preventDefault();
+    clearPoster();
+    // var userSelection = $(e.target).text();
+    var id = $(e.target).data("imdbid");
+    // console.log(userSelection);
+    // console.log(id);
+    
+    var request = {
+      url: "http://www.omdbapi.com/",
+      type: "get",
+      dataType: "json",
+      data: {i: id}
+    }
 
+    var response = $.ajax(request);
 
+      response.done(function(data){
+      // $('#movieData').addClass("border");
+      $('#movieTitle').append(data["Title"]);
+      $('#moviePlot').append(data["Plot"]);
+      $('#moviePoster').append("<img src = '" + data["Poster"] + "'>");
+    })
+    
+  })
+
+  function clearPoster(){
+    $('#movieTitle').empty();
+    $('#moviePlot').empty();
+    $('#moviePoster').empty();
+  }
+  function clearResults(){
+    $('#results').empty();
+    clearPoster();
+  }
 
 });
